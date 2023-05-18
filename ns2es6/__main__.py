3ae488db62c1374f483915118e0c0f114c3dd1ac
Alan Hape <ahape@brightmetrics.com>
2023-05-17 23:51:32 -0700

    ready to test (2)

--- ns2es6/__main__.py
+++ ns2es6/__main__.py
@@ -1,3 +1,3 @@
 from .transforms.sanitize_files import remove_all_comments
 
-remove_all_comments("tests/sanitize.ts")
+remove_all_comments("../tests/sanitize.ts")
--- tests/sanitize.ts
+++ tests/sanitize.ts
@@ -1,7 +1,6 @@
+///  <reference type="something" />
 /// <reference path="something" />
+///<reference path="something" />
 // jshint ignore:true
 // tslint:disable:no-console
-        /// <reference path="something" />
-        // jshint ignore:true
-        // tslint:disable:no-console
 // This is is the only line that should remain
