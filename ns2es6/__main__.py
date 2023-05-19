691cef07530a31e37076d3afe551442051c4d174
Alan Hape <ahape@brightmetrics.com>
2023-05-19 00:05:07 -0700

    update __main__

--- ns2es6/__main__.py
+++ ns2es6/__main__.py
@@ -2,20 +2,22 @@ import os, sys, argparse, logging
 from .transforms import sanitize
 from .utils.logger import logger
 
-dir_ = sys.argv[-1]
-
 def parse_args():
   parser = argparse.ArgumentParser()
   parser.add_argument("directory")
   parser.add_argument("--debug", "-v", action="store_true")
   return parser.parse_args()
 
+def set_logger_level(args):
+  if _args.debug:
+    logger.setLevel(logging.DEBUG)
+  else:
+    logger.setLevel(logging.INFO)
+
 def main(args):
   sanitize.run(args.directory)
 
 if __name__ == "__main__":
   _args = parse_args()
-  logger.setLevel(logging.INFO)
-  if _args.debug:
-    logger.setLevel(logging.DEBUG)
+  set_logger_level(_args)
   main(_args)
