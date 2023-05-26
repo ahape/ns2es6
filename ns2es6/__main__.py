a5fa734b2342948863448edf1fcaefe91077cf76
Alan Hape <ahape@brightmetrics.com>
2023-05-25 22:48:26 -0700

    Test import replacer on BM repo

--- ns2es6/__main__.py
+++ ns2es6/__main__.py
@@ -17,10 +17,10 @@ def set_logger_level(args):
     logger.setLevel(logging.INFO)
 
 def program(args):
-  #sanitize.run(args.directory)
-  #collect_exports.run(args.directory)
   apply_pre_patches(args)
-  replace_imports.run(args.directory)
+  sanitize.run(args.directory, True)
+  #collect_exports.run(args.directory)
+  #replace_imports.run(args.directory)
   #replace_imports.update_file("tests/replace_imports_02.ts", "tests/expectations/replace_imports_02.ts")
 
 def apply_pre_patches(args):
