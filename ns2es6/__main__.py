c0666b1bd847d34372ea8dd9b5791533204fdbad
Alan Hape <ahape@brightmetrics.com>
2023-06-11 22:29:07 -0700

    Move global step to /post

--- ns2es6/__main__.py
+++ ns2es6/__main__.py
@@ -1,10 +1,12 @@
-import os, sys, argparse, logging, json
+import os, sys, shutil, argparse, logging, json
 from ns2es6.transforms import (sanitize,
                                collect_exports,
                                replace_imports,
                                fully_qualify,
                                replace_qualified_with_import)
 from ns2es6.utils.logger import logger
+
+PROJ_PATH = "/Users/alanhape/Projects/ns2es6"
 
 def parse_args():
   parser = argparse.ArgumentParser()
@@ -38,7 +40,7 @@ def program(args):
 
 def apply_pre_patches():
   # TODO Eventually need to auto-set a "git tag" (and remove on clean())
-  for root, _, files in os.walk("/Users/alanhape/Projects/ns2es6/pre"):
+  for root, _, files in os.walk(os.path.join(PROJ_PATH, "pre")):
     for patch_file in sorted(files):
       full_path = os.path.join(root, patch_file)
       logger.info("Applying patch %s", patch_file)
@@ -46,67 +48,8 @@ def apply_pre_patches():
       os.system(f'git commit --quiet -am "{patch_file}"')
 
 def add_globals(directory):
-  with open(os.path.join(directory, "ts", "global.d.ts"), "w", encoding="utf8") as f:
-    f.write(r"""
-import { IPageDefinition } from "Brightmetrics/Interfaces/ipagedefinition";
-import { IBrightCompany } from "Brightmetrics/Interfaces/ibrightcompany";
-import { IBrightUser } from "Brightmetrics/Interfaces/ibrightuser";
-import { IAppInfo } from "Brightmetrics/Interfaces/iappinfo";
-import { IDataSourceGroup } from "Brightmetrics/Interfaces/idatasourcegroup";
-import { IDataConnectionGroupTeir } from "Brightmetrics/DataSources/Interfaces/idataconnectiongroupteir";
-import { IDashboardTab } from "Brightmetrics/Interfaces/idashboardtab";
-import { IBrightPermission } from "Brightmetrics/Interfaces/ibrightpermission";
-import { INotice } from "Brightmetrics/Interfaces/inotice";
-import { DataHostRegion } from "Brightmetrics/Enums/datahostregion";
-declare global {
-  declare const _: _.UnderscoreStatic;
-  declare const Brightmetrics: any;
-  declare const sidebarPages: IPageDefinition[];
-  declare const companyInfo: {
-      success: boolean;
-      logofile?: string;
-      company: IBrightCompany;
-  };
-  declare const userInfo: IBrightUser;
-  declare const appInfo: IAppInfo;
-  declare const dataConnectionGroups: IDataSourceGroup[];
-  declare const defCompanyId: number;
-  declare const myUserId: number;
-  declare const minWaitTime: number;
-  declare const sessionToken: string | undefined;
-  declare const isPublicDashboardPage: boolean | undefined;
-  declare const supportedTimeZones: Array<{ id: string, label: string }>;
-  declare const windowsZones: {
-      supportedTimeZones: Array<{ id: string, label: string }>,
-      tzmapping: Array<[string, Array<[string, string, boolean]>]>
-  };
-  declare const dashboardTabs: IDashboardTab[] | undefined;
-  declare const whiteLabelProductName: string;
-  declare const currentDataHostRegion: DataHostRegion;
-  declare const router: IRouter;
-  declare const roleInfo: { role: { Permissions: IBrightPermission[] } };
-  declare const safeSessionStorage: Storage;
-  declare const dataDefinitionTooltips: Record<string, string>;
-  declare let userMessages: INotice[];
-  declare let companyNotices: INotice[];
-  declare const teirLevelInfo: IDataConnectionGroupTeir[];
-  declare const brightmetricsStorageUrl: string | undefined;
-  declare const bugsnagClient: {
-      notify: (err: any, opts: {
-          beforeSend?: (report: any) => void;
-          metaData?: Record<string, any>;
-          severity?: string;
-      }) => void
-  };
-  declare const featureTreatments: {
-      user: { [featureName: string]: string },
-      company: { [featureName: string]: string }
-  };
-  declare const releaseStage: "production" |
-                              "beta" |
-                              "testing" |
-                              "development";
-}""".strip())
+  shutil.copyfile(os.path.join(PROJ_PATH, "post/global.d.ts"),
+                  os.path.join(directory, "ts/global.d.ts"))
 
 def undo_git_changes():
   clean()
---
+++ post/global.d.ts
@@ -0,0 +1,59 @@
+import { IPageDefinition } from "Brightmetrics/Interfaces/ipagedefinition";
+import { IBrightCompany } from "Brightmetrics/Interfaces/ibrightcompany";
+import { IBrightUser } from "Brightmetrics/Interfaces/ibrightuser";
+import { IAppInfo } from "Brightmetrics/Interfaces/iappinfo";
+import { IDataSourceGroup } from "Brightmetrics/Interfaces/idatasourcegroup";
+import { IDataConnectionGroupTeir } from "Brightmetrics/DataSources/Interfaces/idataconnectiongroupteir";
+import { IDashboardTab } from "Brightmetrics/Interfaces/idashboardtab";
+import { IBrightPermission } from "Brightmetrics/Interfaces/ibrightpermission";
+import { INotice } from "Brightmetrics/Interfaces/inotice";
+import { DataHostRegion } from "Brightmetrics/Enums/datahostregion";
+declare global {
+  declare const _: _.UnderscoreStatic;
+  declare const Brightmetrics: any;
+  declare const sidebarPages: IPageDefinition[];
+  declare const companyInfo: {
+      success: boolean;
+      logofile?: string;
+      company: IBrightCompany;
+  };
+  declare const userInfo: IBrightUser;
+  declare const appInfo: IAppInfo;
+  declare const dataConnectionGroups: IDataSourceGroup[];
+  declare const defCompanyId: number;
+  declare const myUserId: number;
+  declare const minWaitTime: number;
+  declare const sessionToken: string | undefined;
+  declare const isPublicDashboardPage: boolean | undefined;
+  declare const supportedTimeZones: Array<{ id: string, label: string }>;
+  declare const windowsZones: {
+      supportedTimeZones: Array<{ id: string, label: string }>,
+      tzmapping: Array<[string, Array<[string, string, boolean]>]>
+  };
+  declare const dashboardTabs: IDashboardTab[] | undefined;
+  declare const whiteLabelProductName: string;
+  declare const currentDataHostRegion: DataHostRegion;
+  declare const router: IRouter;
+  declare const roleInfo: { role: { Permissions: IBrightPermission[] } };
+  declare const safeSessionStorage: Storage;
+  declare const dataDefinitionTooltips: Record<string, string>;
+  declare let userMessages: INotice[];
+  declare let companyNotices: INotice[];
+  declare const teirLevelInfo: IDataConnectionGroupTeir[];
+  declare const brightmetricsStorageUrl: string | undefined;
+  declare const bugsnagClient: {
+      notify: (err: any, opts: {
+          beforeSend?: (report: any) => void;
+          metaData?: Record<string, any>;
+          severity?: string;
+      }) => void
+  };
+  declare const featureTreatments: {
+      user: { [featureName: string]: string },
+      company: { [featureName: string]: string }
+  };
+  declare const releaseStage: "production" |
+                              "beta" |
+                              "testing" |
+                              "development";
+}
